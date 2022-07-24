542b9c9a7fba9ea7604756307fac8b2f131b0e4c
RitikSinha <ritikk910@gmail.com>
2022-07-24 23:33:31 +0530

    change mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Wrapper = styled.div`
 
   flex-wrap: wrap;
   justify-content: space-between;
-  @media ${devices.tablet} {
+  @media ${devices.mobileL} {
   }
 `;
 export const Home = () => {
